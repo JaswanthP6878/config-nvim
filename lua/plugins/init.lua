4e251d91866c2994dfd4e42a07158a5b4c24abc0
Jaswanth Pinnepu <jaswanth.pinnepu8@gmail.com>
2026-02-24 00:52:19 -0500

    updating nvim config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -216,19 +216,45 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function() 
-        local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files)
-        vim.keymap.set("n", "<leader>fg", builtin.live_grep)
-        vim.keymap.set("n", "<leader>fb", builtin.buffers)
-        vim.keymap.set("n", "<leader>f.", function()
-          require("telescope.builtin").find_files({
-            cwd = vim.fn.expand("%:p:h"),
-          })
-    end)
-    vim.keymap.set("n", "gr", builtin.lsp_references)
-    vim.keymap.set("n", "gi", builtin.lsp_implementations)
-    vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols)
+    config = function()
+      local telescope = require("telescope")
+      local builtin = require("telescope.builtin")
+
+      telescope.setup({
+        defaults = {
+          file_ignore_patterns = {
+            "node_modules/",
+            ".git/",
+            "dist/",
+            "build/",
+            "target/",
+          },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = false,
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+      vim.keymap.set("n", "<leader>f.", function()
+        builtin.find_files({
+          cwd = vim.fn.expand("%:p:h"),
+        })
+      end, { desc = "Find files near current buffer" })
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Recent files" })
+      vim.keymap.set("n", "<leader>fr", builtin.resume, { desc = "Resume picker" })
+      vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find in current buffer" })
+      vim.keymap.set("n", "<leader>fp", builtin.git_files, { desc = "Find git files" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Help tags" })
+      vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
+      vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "LSP references" })
+      vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "LSP implementations" })
+      vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { desc = "Document symbols" })
     end,
   },
 
